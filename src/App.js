dfb6b49315e030ed4d96cfc22379d334a661bb78
ramezmezni16 <ramez.nabila@gmail.com>
2025-01-29 10:28:50 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./components/Login/Login"
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Routes>
+        <Route path='/Home' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
         <Login />
       </BrowserRouter>
     </div>
