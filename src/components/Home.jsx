cb5af57d2e9e47f73e1a065e740ca616e30355a2
ramezmezni16 <ramez.nabila@gmail.com>
2025-01-30 18:04:05 +0100

    tailwind css installation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
   )
 }
 
